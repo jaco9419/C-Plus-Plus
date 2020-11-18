185ef6e72ec0f76cab2e0c34fe4a3a6bb3ff613d
jaco9419 <jaco.9419@gmail.com>
2020-11-18 10:27:15 -0300

    Create adding calculator

---
+++ First Projects/2) Console Calculator/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double num1;
+    double num2;
+
+    cout << "Enter First Number: ";
+    cin >> num1;
+
+    cout << "Enter Second Number: ";
+    cin >> num2;
+
+    cout << num1 + num2;
+    return 0;
+}
